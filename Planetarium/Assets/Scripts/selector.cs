855f3e8a0fbf950c0ade9cc87b6eea7a622a3cfa
terfno <sueda.takahito@gmail.com>
2018-07-15 22:43:08 +0900

    add script

--- Planetarium/Assets/Scripts/selector.cs
+++ Planetarium/Assets/Scripts/selector.cs
@@ -2,79 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * セレクター用のレーザーポインターを出すクラス
+ */
 public class selector : MonoBehaviour {
 
-	// Use this for initialization
 	[SerializeField]
-	private Transform _LeftHandAnchor;
-
-	[SerializeField]
-	private Transform _RightHandAnchor;
+	private Transform _RightHandAnchor; // 右手
 
 	[SerializeField]
-	private Transform _CenterEyeAnchor;
+	private Transform _LeftHandAnchor;  // 左手
 
 	[SerializeField]
-	private float _MaxDistance = 100.0f;
-	private float _MinDistance = 0.0f;
+	private Transform _CenterEyeAnchor; // 目の中心
 
 	[SerializeField]
+	private float _MaxDistance = 100.0f; // 距離
 
-	private LineRenderer _LaserPointerRenderer;
+	[SerializeField]
+	private LineRenderer _LaserPointerRenderer; // LineRenderer
 
+	// コントローラー
 	private Transform Pointer {
-        get {
-            // 現在アクティブなコントローラーを取得
-            var controller = OVRInput.GetActiveController();
-            if (controller == OVRInput.Controller.RTrackedRemote) {
-                return _RightHandAnchor;
-            } else if (controller == OVRInput.Controller.LTrackedRemote) {
-                return _LeftHandAnchor;
-            }
-            // どちらも取れなければ目の間からビームが出る
-            return _CenterEyeAnchor;
-        }
-    }
-
-	void Start () {
-		
+		get {
+			// 現在アクティブなコントローラーを取得
+			var controller = OVRInput.GetActiveController();
+			if (controller == OVRInput.Controller.RTrackedRemote) {
+				return _RightHandAnchor;
+			} else if (controller == OVRInput.Controller.LTrackedRemote) {
+				return _LeftHandAnchor;
+			}
+			// どちらも取れなければ目の間からビームが出る
+			return _CenterEyeAnchor;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		var pointer = Pointer;
-        if (pointer == null || _LaserPointerRenderer == null)
-        {
-            return;
-        }
-        // コントローラーのトリガー
-        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
-            {
-            // コントローラー位置からRayを飛ばす
-            Ray pointerRay = new Ray(pointer.position, pointer.forward);
-            // レーザーの起点
-            _LaserPointerRenderer.SetPosition(0, pointerRay.origin);
 
-            RaycastHit hitInfo;
-            if (Physics.Raycast(pointerRay, out hitInfo, _MaxDistance))
-            {
-                // Rayがヒットしたらそこまで
-                _LaserPointerRenderer.SetPosition(1, hitInfo.point);
-            }
-            else
-            {
-                // Rayがヒットしなかったら向いている方向にMaxDistance伸ばす
-                _LaserPointerRenderer.SetPosition(1, pointerRay.origin + pointerRay.direction * _MaxDistance);
-            }
-        }
-        else
-        {
-            // コントローラー位置からRayを飛ばす
-            Ray pointerRay = new Ray(pointer.position, pointer.forward);
-            // レーザーの起点
-            _LaserPointerRenderer.SetPosition(0, pointerRay.origin);
-            // コントローラーのトリガー無しの場合、Rayが向いている方向にMinDistance（０）
-            _LaserPointerRenderer.SetPosition(1, pointerRay.origin + pointerRay.direction * _MinDistance);
-        }
+	void Update () {
+		var pointer = Pointer; // コントローラーを取得
+		// コントローラーがない or LineRendererがなければ何もしない
+		if (pointer == null || _LaserPointerRenderer == null) {
+			return;
+		}
+		// コントローラー位置からRayを飛ばす
+		Ray pointerRay = new Ray(pointer.position, pointer.forward);
+
+		// レーザーの起点
+		_LaserPointerRenderer.SetPosition(0, pointerRay.origin);
+
+		RaycastHit hitInfo;
+		if (Physics.Raycast(pointerRay, out hitInfo, _MaxDistance)) {
+			// Rayがヒットしたらそこまで
+			_LaserPointerRenderer.SetPosition(1, hitInfo.point);
+		} else {
+			// Rayがヒットしなかったら向いている方向にMaxDistance伸ばす
+			_LaserPointerRenderer.SetPosition(1, pointerRay.origin + pointerRay.direction * _MaxDistance);
+		}
 	}
 }
